264452dce5ef8bdd316eee97b8a8fb4e2cc84aff
alefabian17 <alewacho17@gmail.com>
2025-09-03 12:16:26 -0400

    mi archivo operacion python

---
+++ operacion.py
@@ -0,0 +1,10 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1 Alex Fabian Rodriguez - CI: 9909074")   # Autor del archivo
+print("2 Arlin Aviles - CI: 12668362")
+print("3 Gabriel Cautile - CI: 13182157")
+print("4 José Yujra - CI: 12796351")
