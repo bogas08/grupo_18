2c94abe3fe08d0e07ca3a02e132fb38359bb34fc
jose yujra <jyujra@566gmail.com>
2025-09-03 12:20:25 -0400

    mi archivo saludo java

---
+++ saludo.java
@@ -0,0 +1,10 @@
+public class saludo {
+    public static void main(String[]args) {
+        System.err.println("Hola, Equipo");
+        System.out.println("1 Yujra Arce Jose Armando");
+        System.out.println("2 Cutile Lopez Gabriel Zeus");
+        System.out.println("3 Aviles Gonzales Arlin");
+        System.out.println("4 Fabian Rodriguez Jhamil Alex");
+    }
+
+}
